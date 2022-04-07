9e45514509b704af5da0c0b0b9e6f71eb7470230
HoustonSprouse919 <89546823+HoustonSprouse919@users.noreply.github.com>
2022-04-07 15:54:37 -0400

    Update HomeView.vue

--- testing/src/views/HomeView.vue
+++ testing/src/views/HomeView.vue
@@ -10,40 +10,124 @@ document.title = "Home";
       website designed for the modern<br>
       generation of social drinking!</p>
   </div>
-  <div class="wip">
-    <img src="../assets/images/Image_1.png">
+  <div>
+  <img id = "background1" src = "https://pbs.twimg.com/media/Drz_T0TUcAA0ugT?format=jpg&name=4096x4096">
   </div>
+  <div class = "Bottom-bar"> </div>
+    <div class="announcements">
+      <h1>New Changes</h1>
+      <p1>Website launch!</p1>
+       <H3>Today! </H3>
+      <p3> Random Cocktail Function Implemented!</p3>
+      <h2>News</h2>
+      <p5>Dev update messages go here blah blah <br>
+        blah blah blah</p5>
+
+  </div>
+
 
 </template>
 
 <style scoped>
 
 .mainMessage{
-  height: 300px;
-  width: 550px;
+  height: 250px;
+  width: 500px;
   position: absolute;
   border-radius: 15px;
   right: 100px;
   top: 200px;
-  background: rgba(0, 0, 0, .4);
+  background: rgba(0, 0, 0, .3);
   color: white;
   text-align: center;
 }
-
+.mainMessage h1{
+  font-size: 35px;
+  position: relative;
+  bottom: -20px;
+  text-shadow: 2px 2px 4px #000000;
+}
 .mainMessage p{
   font-size: 25px;
   position: relative;
-  bottom: -20px;
+  bottom: -10px;
+  text-shadow: 2px 2px 4px #000000;
 }
 
-.wip img{
-  position: relative;
-  width: 600px;
-  height: auto;
-  top: 80px;
-  left: 50px;
-  border-radius: 15px;
-  border: 3px black solid;
+
+#background1 {
+  height: 100%;
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  position: absolute;
+  bottom: 1px;
+  z-index: -300;
 }
 
+.Bottom-bar{
+  width: 1500px;
+  height: 200px;
+  margin-top: 550px;
+  float: left;
+  color: white;
+  text-align: center;
+  background-color: rgba(0, 0, 0, .7);
+}
+.announcements{
+  width: 1500px;
+  height: 200px;
+  margin-top: 500px;
+  float: left;
+  color: white;
+  text-align: center;
+  background-color: rgba(0, 0, 0, .7);
+}
+.announcements{
+  margin-top: 20px;
+}
+.announcements h1{
+  font-size: 25px;
+  position: absolute;
+  bottom: 50px;
+  right: 1300px;
+  text-shadow: 2px 2px 4px #000000;
+}
+.announcements h2{
+  font-size: 25px;
+  position: absolute;
+  bottom: 50px;
+  left: 1200px;
+}
+.announcements p1{
+  font-size: 20px;
+  position: absolute;
+  bottom: 35px;
+  right: 1310px;
+  color: gray;
+  text-shadow: 2px 2px 4px #000000;
+}
+.announcements h3{
+  font-size: 25px;
+  position: absolute;
+  bottom: 40px;
+  right: 1100px;
+  text-shadow: 2px 2px 4px #000000;
+}
+.announcements p3{
+  font-size: 20px;
+  position: absolute;
+  bottom: 35px;
+  right: 850px;
+  color: gray;
+  text-shadow: 2px 2px 4px #000000;
+}
+.announcements p5{
+  font-size: 20px;
+  position: absolute;
+  bottom: 20px;
+  left: 1200px;
+  color: gray;
+  text-shadow: 2px 2px 4px #000000;
+}
 </style>
